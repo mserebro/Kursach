38f099dd9d89736da7e7da6870bf9b555b09bcf6
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-11-20 15:03:22 +0300

    Update views.py

--- mysite/liblinks/views.py
+++ mysite/liblinks/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 
 
+field = ["Статья из сборника","Фамилии и инициалы авторов (через запятую)","Заголовок статьи из сборника","Название сборника","Город, в котором издан сборник","Издательство","Год издания","Страницы"]
+
 def index(request):
     if request.GET:
         print(request.GET)
     
-    return render(request, 'liblinks/index.html')
+    return render(request, 'liblinks/index.html',{'field': field, 'title': 'Оформление библиографических ссылок'})
     
     return HttpResponse("<h1>Страница библиографических ссылок</h1>")
     
