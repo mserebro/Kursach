5a035078cb9ecf7f29764b9feac836c881043717
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-11-20 14:33:58 +0300

    Update views.py

--- mysite/liblinks/views.py
+++ mysite/liblinks/views.py
@@ -1,11 +1,13 @@
 
-
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 
 
 def index(request):
     if request.GET:
         print(request.GET)
+    
+    return render(request, '')
     
     return HttpResponse("<h1>Страница библиографических ссылок</h1>")
     
