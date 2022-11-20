2984daa2b6c64980c8641b64f21f65dfdbe9d5be
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-11-20 11:02:57 +0300

    Create 0001_initial.py

---
+++ mysite/liblinks/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='News',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=150)),
+                ('content', models.TextField(blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('is_published', models.BooleanField(default=True)),
+            ],
+        ),
+    ]
