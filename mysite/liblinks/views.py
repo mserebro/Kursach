b9e043d32470d8b6accdd24ee4f10c4b79c85e58
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-11-20 10:13:01 +0300

    Update views.py

--- mysite/liblinks/views.py
+++ mysite/liblinks/views.py
@@ -1,8 +1,15 @@
 
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
-def index(request)
-    #print(request)
+def index(request):
+    if request.GET:
+        print(request.GET)
+    
     return HttpResponse("<h1>Страница библиографических ссылок</h1>")
+    
+    
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+    
