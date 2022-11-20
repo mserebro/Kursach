716d548cacee1baf2a2b6088e9903a93ce75c293
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-11-20 10:35:40 +0300

    Update models.py

--- mysite/liblinks/models.py
+++ mysite/liblinks/models.py
@@ -1,1 +1,11 @@
 
+from django.db import models
+
+class Liblinks(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    views = models.IntegerField(default=0)
