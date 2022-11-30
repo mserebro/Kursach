34bb0b3722a1ab853300dc12079ed921ee9ecd09
Mark0vAlexander <99321113+Mark0vAlexander@users.noreply.github.com>
2022-11-30 16:04:29 +0300

    Update urls.py

--- mysite/liblinks/urls.py
+++ mysite/liblinks/urls.py
@@ -4,9 +4,11 @@ from .views import *
 
 urlpatterns = [
    path('', index, name='home'),
-   path('addliblink/', addliblink, name='add_liblink'),
-   path('contact/', contact, name='contact'),
-   path('login/', login, name='login'),
-   path('post/<int:post_id>/', show_post, name='post'),
+   # path('addliblink/', addliblink, name='add_liblink'),
+   # path('contact/', contact, name='contact'),
+   # path('login/', login, name='login'),
+   # path('post/<int:post_id>/', show_post, name='post'),
+   path('liblinks/', LiblinksView/as_view(), name='Liblinks'),
+   path('liblinks/<int:liblinks_id>/', LiblinksUpdate/as_view(), name='LiblinksUpdate'),
 ]
 
